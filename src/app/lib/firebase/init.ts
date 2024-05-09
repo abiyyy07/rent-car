32dc6862582136a7f015286dd96c2ff678fec075
abiyyy07 <abiyy0206@gmail.com>
2024-05-09 21:46:53 +0700

    Initialize Firebase

---
+++ src/app/lib/firebase/init.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIESZS2MA_-jJl5OHwQstRzAb8aoQXqDg",
+  authDomain: "rent-car-5a44b.firebaseapp.com",
+  projectId: "rent-car-5a44b",
+  storageBucket: "rent-car-5a44b.appspot.com",
+  messagingSenderId: "658594720889",
+  appId: "1:658594720889:web:c7f0c2318a92126f9a38a8"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/app/lib/firebase/service.ts
@@ -0,0 +1,24 @@
+import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
+import app from "./init";
+import bcrypt from 'bcrypt'
+import { getStorage } from "firebase/storage";
+
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+export default storage;
+
+export async function retrieveData(collectionName: string) {
+    const snapshot = await getDocs(collection(firestore, collectionName))
+    const data = snapshot.docs.map((doc) => ({
+        id:doc.id,
+        ...doc.data()
+    }))
+
+    return data;
+}
+
+export async function retrieveDataById(collectionName: string, id: string) {
+    const snapshot = await getDoc(doc(firestore, collectionName, id))
+    const data = snapshot.data()
+    return data;
+}
